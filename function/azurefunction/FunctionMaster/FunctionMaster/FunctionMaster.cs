66ce295b18239b6600c830f4b92c32ffd2d4cadb
vinhtq10 <vinhtq.itv365@gmail.com>
2020-04-24 16:43:00 +0700

    Revert "commit fucntion netcore"
    This reverts commit 3b1845ec09c3e74d39c38a1abd6b9772c3ad4c7e.

--- function/azurefunction/FunctionMaster/FunctionMaster/FunctionMaster.cs
+++ function/azurefunction/FunctionMaster/FunctionMaster/FunctionMaster.cs
@@ -12,7 +12,7 @@ namespace FunctionMaster
 {
     public static class FunctionMaster
     {
-        [FunctionName("FunctionOne")]
+        [FunctionName("FunctionMaster")]
         public static async Task<IActionResult> Run(
             [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
             ILogger log)
